ebf98ba81f477e36bf362107367e67e508c4c6b4
Smerdokryl <smerdokryl@gmail.com>
2024-08-12 00:03:07 +0300

    Redesign to generate buttons semiautomatically

--- ChatMenuUnpacked.lua
+++ ChatMenuUnpacked.lua
@@ -1,59 +1,61 @@
-local function SubmenuItemsSetOnClick(submenu, func)
-	for i=1,submenu.numButtons do
-		_G[submenu:GetName()..'Button'..i]:SetScript('OnClick', func);
-	end
+ChatMenuUnpackedMixin = {};
+
+function ChatMenuUnpackedMixin:OnLeaveMenu()
+	local menu = _G[self.nested];
+	menu:Hide();
+	menu:ClearAllPoints();
+	menu:SetParent(self.restore.parent);
+	menu:SetScript('OnLeave', self.restore.OnLeave);
 end
 
-function ChatFrame_ToggleSubMenu(button, submenu, onclick, onclick_override)
-	local isShown = submenu:IsShown();
+function ChatMenuUnpackedMixin:OnClick()
+	PlaySound(SOUNDKIT.IG_CHAT_EMOTE_BUTTON);
 
+	local menu = _G[self.nested];
+	if menu:IsShown() then
+		self:OnLeaveMenu();
+		return;
+	end
+
+	-- Little hack to hide all other menus
+	ChatMenu:Show();
 	ChatMenu:Hide();
-	LanguageMenu:Hide();
-	EmoteMenu:Hide();
-	VoiceMacroMenu:Hide();
 
-	if isShown then
-		submenu:Hide();
-		submenu:SetParent(ChatMenu);
-		SubmenuItemsSetOnClick(submenu, onclick);
-	else
-		submenu:SetParent(button);
-		submenu:ClearAllPoints();
-		submenu:SetPoint('BOTTOMLEFT', button, 'BOTTOMRIGHT');
-		SubmenuItemsSetOnClick(submenu, onclick_override);
-		submenu:Show();
-	end
-end
-
-
--- LanguageMenu
-function UnpackedLanguageMenu_Click(self)
-	ChatMenu.chatFrame.editBox.language, ChatMenu.chatFrame.editBox.languageID = GetLanguageByIndex(self:GetID());
-	LanguageMenu:Hide();
-end
-
--- EmoteMenu
-function UnpackedEmoteMenu_Click(self)
-	DoEmote(EmoteList[self:GetID()]);
-	EmoteMenu:Hide();
-end
-
--- VoiceMacroMenu
-function UnpackedVoiceMacroMenu_Click(self)
-	local emote = TextEmoteSpeechList[self:GetID()];
-	if (emote == EMOTE454_TOKEN or emote == EMOTE455_TOKEN) then
-		local faction = UnitFactionGroup("player", true);
-		if (faction == "Alliance") then
-			emote = EMOTE454_TOKEN;
-		elseif (faction == "Horde") then
-			emote = EMOTE455_TOKEN;
+	menu:ClearAllPoints();
+	menu:SetParent(self);
+	menu:SetPoint('BOTTOMLEFT', self, 'TOPRIGHT');
+	menu:SetScript('OnLeave', function(self, motion)
+		if motion and not self:IsMouseOver() then
+			self:GetParent():OnLeaveMenu()
 		end
-	end
-
-	DoEmote(emote);
-	VoiceMacroMenu:Hide();
+	end);
+	menu:Show();
 end
 
--- Adjust minimum height of chat window
-local w, h = ChatMenu.chatFrame:GetResizeBounds();
-ChatMenu.chatFrame:SetResizeBounds(w, h + 38);
+local ChatMenuUnpacked = {
+	{ ChatMenuButton12, "\195\134",     {'Fonts\\morpheus.ttf', 18, ''}               }, -- Language "Æ"
+	{ ChatMenuButton11, "\226\153\171", {'Fonts\\arialn.ttf',   18, ''}               }, -- Voice Emote "♫"
+	{ ChatMenuButton10, "\226\152\187", {'Fonts\\arialn.ttf',   22, ''}, {-1.5, 2.25} }  -- Emote "☻"
+};
+
+local anchor = ChatFrameMenuButton;
+for _, entry in ipairs(ChatMenuUnpacked) do
+	local ChatMenuButton, text, font, textPos = unpack(entry);
+	local button = CreateFrame("Button", nil, ChatFrame1ButtonFrame, 'ChatFrameUnpackedMenuButtonTemplate');
+	button.chatFrame = ChatMenu.chatFrame;
+	button.nested = ChatMenuButton.nested;
+
+	local menu = _G[button.nested];
+	button.restore = {
+		parent  = menu:GetParent(),
+		OnLeave = menu:GetScript('OnLeave')
+	};
+
+	button:SetText(text);
+	button.Text:SetTextColor(207/255, 175/255, 16/255);
+	if font then button.Text:SetFont(unpack(font)) end
+	if textPos then button.Text:SetPoint('CENTER', button, 'CENTER', unpack(textPos)) end
+
+	button:SetPoint('BOTTOM', anchor, 'TOP', 0, 2);
+	anchor = button;
+end
